a5ef605c097ec01ed90d5918d4bc0ecafc706b59
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2022-09-03 16:55:39 +0400

    Added timestamps and added column annotations to Account model

--- src/main/java/com/example/minibank/account/Account.java
+++ src/main/java/com/example/minibank/account/Account.java
@@ -1,8 +1,11 @@
 package com.example.minibank.account;
 
 import com.example.minibank.customer.Customer;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "accounts")
@@ -12,13 +15,22 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Column(name = "code", nullable = false)
     private String code;
 
+    @Column(name = "balance", nullable = false)
     private double balance;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     @OneToOne
     @JoinColumn(name = "customer_id", referencedColumnName = "customer_id")
     private Customer customer;
-
 
 }
