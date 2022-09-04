416e026db4077b42159525c31ef78d918fb10803
Shahlin Ibrahim <32275018+shahlin@users.noreply.github.com>
2022-09-04 13:22:02 +0400

    Added unit tests for account transfers listing and deposits

--- src/test/java/com/example/minibank/service/AccountServiceTest.java
+++ src/test/java/com/example/minibank/service/AccountServiceTest.java
@@ -1,9 +1,12 @@
 package com.example.minibank.service;
 
+import com.example.minibank.controller.request.DepositRequest;
 import com.example.minibank.exception.AccountExistsException;
 import com.example.minibank.exception.AccountNotFoundException;
+import com.example.minibank.exception.AccountTransactionException;
 import com.example.minibank.model.Account;
 import com.example.minibank.model.Customer;
+import com.example.minibank.model.Transfer;
 import com.example.minibank.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,12 +16,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -57,6 +60,24 @@ class AccountServiceTest {
     }
 
     @Test
+    void canGetAllTransfersForNewAccount() {
+        String code = anyString();
+
+        Account account = new Account();
+        account.setId(1);
+        account.setCode(code);
+        account.setBalance(0);
+
+        when(accountRepository.findAccountByCode(code)).thenReturn(Optional.of(account));
+
+        Map<String, List<Transfer>> expectedTransfers = new HashMap<>();
+        expectedTransfers.put("sent", Collections.emptyList());
+        expectedTransfers.put("received", Collections.emptyList());
+
+        assertThat(accountService.getAllTransfers(code)).isEqualTo(expectedTransfers);
+    }
+
+    @Test
     void canOpenNewAccountForCustomerWithExistingAccount() {
         String randomCode = UUID.randomUUID().toString();
         Integer id = 1;
@@ -78,17 +99,41 @@ class AccountServiceTest {
     }
 
     @Test
-    void willThrowAccountNotFoundWhenAccountDoesNotExist() {
-        String code = UUID.randomUUID().toString();
+    void canDepositValidAmountIntoAccount() {
+        double amountToDeposit = 1000;
 
+        DepositRequest depositRequest = new DepositRequest();
+        depositRequest.setAmount(amountToDeposit);
+
+        String code = anyString();
         Account account = new Account();
+        account.setId(1);
         account.setCode(code);
+        account.setBalance(0);
 
-        assertThrows(AccountNotFoundException.class, () -> accountService.getAccount(code));
+        when(accountRepository.findAccountByCode(code)).thenReturn(Optional.of(account));
+
+        accountService.deposit(code, depositRequest);
+
+        assertThat(account.getBalance()).isEqualTo(amountToDeposit);
     }
 
     @Test
-    void willThrowAccountExistsIfCustomerAlreadyHasAnAccount() {
+    void willThrowWhenAccountDoesNotExistOnGetSingleAccount() {
+        when(accountRepository.findAccountByCode(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class, () -> accountService.getAccount(anyString()));
+    }
+
+    @Test
+    void willThrowWhenAccountDoesNotExistOnGetAllTransfers() {
+        when(accountRepository.findAccountByCode(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class, () -> accountService.getAllTransfers(anyString()));
+    }
+
+    @Test
+    void willThrowWhenCustomerAlreadyHasAnAccount() {
         String randomCode = UUID.randomUUID().toString();
         Integer id = 1;
 
@@ -106,5 +151,51 @@ class AccountServiceTest {
         when(accountRepository.findAccountByCustomerId(id)).thenReturn(Optional.of(account));
 
         assertThrows(AccountExistsException.class, () -> accountService.openNewAccountForCustomer(customer));
+    }
+
+    @Test
+    void willThrowWhenAccountDoesNotExistOnDeposit() {
+        DepositRequest depositRequest = new DepositRequest();
+        depositRequest.setAmount(1000);
+
+        when(accountRepository.findAccountByCode(anyString())).thenReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class, () -> accountService.deposit(anyString(), depositRequest));
+    }
+
+    @Test
+    void willThrowWhenDepositAmountIsMoreThanAllowed() {
+        double amountToDeposit = 100_000_000;
+
+        DepositRequest depositRequest = new DepositRequest();
+        depositRequest.setAmount(amountToDeposit);
+
+        String code = anyString();
+        Account account = new Account();
+        account.setId(1);
+        account.setCode(code);
+        account.setBalance(0);
+
+        when(accountRepository.findAccountByCode(code)).thenReturn(Optional.of(account));
+
+        assertThrows(AccountTransactionException.class, () -> accountService.deposit(code, depositRequest));
+    }
+
+    @Test
+    void willThrowWhenDepositAmountIsLessThanAllowed() {
+        double amountToDeposit = -10;
+
+        DepositRequest depositRequest = new DepositRequest();
+        depositRequest.setAmount(amountToDeposit);
+
+        String code = anyString();
+        Account account = new Account();
+        account.setId(1);
+        account.setCode(code);
+        account.setBalance(0);
+
+        when(accountRepository.findAccountByCode(code)).thenReturn(Optional.of(account));
+
+        assertThrows(AccountTransactionException.class, () -> accountService.deposit(code, depositRequest));
     }
 }
